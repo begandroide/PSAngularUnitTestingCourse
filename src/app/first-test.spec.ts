13ab4dc96bf68bec2de9af047d08c48593fda82d
Benjamin Gautier <benjamin.gautier@licklider.cl>
2020-10-11 21:50:52 -0300

    spec pipe, service and component

---
+++ src/app/first-test.spec.ts
@@ -0,0 +1,29 @@
+
+describe('my first test', () => {
+  let sut;
+
+  beforeEach(() => {
+    sut = {};
+  });
+
+  it('should be true if true', () => {
+    // arrange
+    sut.a = false;
+
+    // act
+    sut.a = true;
+
+    // assert
+    expect(sut.a).toBe(true);
+  });
+});
+
+/**
+ * Ejemplo
+ * 'user service getUser method should retrieve the correct user'
+ * describe('user service', () => {
+ *  describe('getUser method', () => {
+ *    it('should retrieve the correct user')
+ *  })
+ * })
+ */
